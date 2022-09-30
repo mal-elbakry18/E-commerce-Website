0d2cd5085df656181374c8f6b84bb604dafd698b
Abdel6602 <abdelrahmangamal66002@gmail.com>
2022-09-30 23:41:09 +0200

    product api admin side done

---
+++ Backend - Api/Models/ProductModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    // tag1: { type: String, required: true },
+    // tag2: { type: String, required: true },
+    // tag3: { type: String, required: true },
+    sizesAvailable: {
+        type: [String],
+        required:false
+    },
+    colorsAvailable: [{
+        type: String,
+        required:true
+    }],
+    price: { type: String },
+    count: {
+      type: Number,
+      default: 1,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    gender: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("products", productSchema);
--- Backend - Api/server.js
+++ Backend - Api/server.js
@@ -5,8 +5,8 @@ const webApp = require('./Routes/web');
 const app = express();
 const PORT = 8080
 
-app.use(adminApp);
-app.use(webApp);
+app.use('/admin', adminApp);
+app.use('/web', webApp);
 
 app.listen(PORT, () =>{
     console.log(`Listening on port ${PORT}`);
